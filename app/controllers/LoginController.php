d29980b320761c8e5806b69a1a68c4d494e52c43
Alba Veloso <albaveloso92@gmail.com>
2025-02-24 20:26:44 +0100

    tiendamvc

---
+++ app/controllers/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+namespace Formacom\controllers;
+use Formacom\Core\Controller;
+use Formacom\Models\User;
+
+
+class LoginController extends Controller{
+    public function index(...$params){
+        
+        $this->view("login");
+    }
+    public function login(...$params){
+        if(isset($_POST["username"])){
+            $user=User::where("username",$_POST["username"])->first();
+            if($user && password_verify($_POST["password"],$user->password)){
+                session_start();
+                $_SESSION["user_id"]=$user->user_id;
+                $_SESSION["username"]=$user->username;
+                header("Location: ".base_url()."admin");
+
+            }else{
+                $error="User or password incorrect";
+                $this->view("login", [$error]);
+            }
+            //var_dump($user->password);
+            
+            exit();
+        }else{
+           header("Location: ".base_url()."login");
+        }
+       
+    }
+
+    public function register(...$params){
+        $this->view("register");
+    }
+
+    public function json(){
+        $actores=Actor::where("first_name","like","P%")->get();
+        $datos=[
+            "mensaje"=>"Listado actores empiezan P",
+            "listado"=>$actores
+        ];
+        $json=json_encode($datos);
+        header('Content-Type: application/json');
+        echo $json;
+        exit();
+    }
+
+}
+?>
